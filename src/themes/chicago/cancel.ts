65ad24515bff42dc4cf36cb480f0ade3685dfe1b
Steven Persia <persia.steven8@gmail.com>
2020-04-24 17:40:17 +0200

    feat(Chicago): create Chicago theme

---
+++ src/themes/chicago/cancel.ts
@@ -0,0 +1,47 @@
+const StyledView = {
+  ul: {
+    margin: 0,
+    padding: 0,
+    listStyleType: "none",
+  },
+
+  ".container": {
+    padding: "25px 25px 25px 50px",
+    fontFamily: "Calibri",
+  },
+
+  ".top": {},
+  ".bottom": {},
+  ".left": {
+    width: 25,
+    height: 1123,
+    background: "tomato",
+    position: "absolute",
+  },
+  ".right": {},
+
+  ".head": { display: "grid" },
+  ".sender": {},
+  ".destin": { textAlign: "right" },
+
+  ".done-at": {
+    paddingTop: 20,
+    textAlign: "right",
+  },
+
+  ".object-info": {
+    padding: "40px 0",
+    fontSize: 18,
+  },
+
+  ".content": {
+    textAlign: "justify",
+  },
+
+  ".signature": {
+    paddingTop: 100,
+    fontStyle: "italic",
+  },
+};
+
+export default StyledView;
