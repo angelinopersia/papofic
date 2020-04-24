c9ebef176928e5dd4b5f4ced15830de87c886ee5
Steven Persia <persia.steven8@gmail.com>
2020-04-24 15:13:29 +0200

    feat(Cancel): rework default theme

--- src/themes/default/cancel.ts
+++ src/themes/default/cancel.ts
@@ -1,108 +1,41 @@
 const StyledView = {
-  ".x": {
-    // border: "1px red dashed",
+  ul: {
+    margin: 0,
+    padding: 0,
+    listStyleType: "none",
   },
 
   ".container": {
-    display: "grid",
-    gridTemplateColumns: "50% 50%",
-    padding: "25px",
-    width: "794px",
-    // height: "1123px",
-    backgroundColor: "white",
-    color: "black",
+    padding: 25,
+    fontFamily: "Calibri",
   },
 
-  ".outer-header": {
-    //
-  },
-  ".inner-header": {
-    //
-  },
+  ".top": {},
+  ".bottom": {},
+  ".left": {},
+  ".right": {},
 
-  ".inner-footing": {
-    //
-  },
-  ".outer-footing": {
-    //
-  },
+  ".head": { display: "grid" },
+  ".sender": {},
+  ".destin": { textAlign: "right" },
 
-  // SENDER
-  ".sender-names": {
-    gridColumn: "1",
-    gridRow: "1",
-  },
-  ".sender-first-name": {
-    //
-  },
-  ".sender-last-name": {
-    //
-  },
-  ".sender-address": {
-    gridColumn: "1",
-    gridRow: "3",
-  },
-  ".sender-postcode": {
-    gridColumn: "1",
-    gridRow: "4",
-  },
-  ".sender-city": {
-    gridColumn: "1",
-    gridRow: "5",
-  },
-  ".sender-phone": {
-    gridColumn: "1",
-    gridRow: "6",
-  },
-  ".sender-email": {
-    gridColumn: "1",
-    gridRow: "7",
-  },
-
-  // DESTINATION
-  ".destin-name": {
-    gridColumn: "2",
-    gridRow: "8",
-    textAlignLast: "right",
-  },
-  ".destin-address": {
-    gridColumn: "2",
-    gridRow: "9",
-    textAlignLast: "right",
-  },
-  ".destin-postcode": {
-    gridColumn: "2",
-    gridRow: "10",
-    textAlignLast: "right",
-  },
-  ".destin-city": {
-    gridColumn: "2",
-    gridRow: "11",
-    textAlignLast: "right",
-  },
-
-  // MISC
   ".done-at": {
-    paddingTop: "20px",
-    textAlignLast: "right",
-    gridColumn: "1 / 3",
+    paddingTop: 20,
+    textAlign: "right",
   },
 
   ".object-info": {
-    padding: "40px 0 40px 0",
-    fontWeight: "bold",
-    gridColumn: "1 / 3",
+    padding: "40px 0",
+    fontWeight: 700,
   },
 
-  // CONTENT
   ".content": {
-    gridColumn: "1 / 3",
+    textAlign: "justify",
   },
 
   ".signature": {
-    paddingTop: "100px",
+    paddingTop: 100,
     fontStyle: "italic",
-    gridColumn: "1 / 3",
   },
 };
 
