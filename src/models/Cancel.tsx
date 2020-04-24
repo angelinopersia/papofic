dcc2769f67f77ee05b591e464659fdf6bc919f2f
P. A <lowd70@gmail.com>
2020-04-24 11:09:36 +0200

    feat(*): Added and improved lots of things except changing themes

--- src/models/Cancel.tsx
+++ src/models/Cancel.tsx
@@ -7,101 +7,172 @@ type CancelVType = {
 
 const sender = {
   title: "Emetteur",
+  id: "sender",
   items: [
-    { title: "Prénom", name: "sFirstName" },
-    { title: "Nom", name: "sLastName" },
-    { title: "Adresse", name: "sAddress" },
-    { title: "Code Postal", name: "sPostcode" },
-    { title: "Ville", name: "sCity" },
-    { title: "Numéro de téléphone", name: "sPhone" },
-    { title: "Adresse email", name: "sEmail" },
+    { title: "Prénom", name: "sFirstName", icon: "person" },
+    { title: "Nom", name: "sLastName", icon: "person" },
+    { title: "Adresse", name: "sAddress", icon: "home", fullRow: true },
+    {
+      title: "Code Postal",
+      name: "sPostcode",
+      icon: "map-marker",
+    },
+    { title: "Ville", name: "sCity", icon: "map-marker" },
+    {
+      title: "Numéro de téléphone",
+      name: "sPhone",
+      icon: "phone",
+    },
+    { title: "Adresse email", name: "sEmail", icon: "envelope" },
   ],
 };
 const destination = {
   title: "Destinataire",
+  id: "destination",
   items: [
-    { title: "Destinataire", name: "dName" },
-    { title: "Adresse du destinataire", name: "dAddress" },
-    { title: "Code postal du destinataire", name: "dPostcode" },
-    { title: "Ville du destinataire", name: "dCity" },
+    { title: "Destinataire", name: "dName", icon: "person", fullRow: true },
+    {
+      title: "Adresse",
+      name: "dAddress",
+      icon: "home",
+      fullRow: true,
+    },
+    {
+      title: "Code postal",
+      name: "dPostcode",
+      icon: "map-marker",
+    },
+    {
+      title: "Ville",
+      name: "dCity",
+      icon: "map-marker",
+    },
   ],
 };
 const misc = {
   title: "Divers",
+  id: "misc",
   items: [
-    { title: "Faite à (Ville)", name: "mCity" },
-    { title: "Faite le (Date)", name: "mDate" },
-    { title: "Objet", name: "mObjet" },
+    {
+      title: "Faite à (Ville)",
+      name: "mCity",
+      icon: "map-marker",
+    },
+    {
+      title: "Faite le (Date)",
+      name: "mDate",
+      icon: "calendar",
+      type: "date",
+    },
+    { title: "Objet", name: "mObjet", icon: "edit", fullRow: true },
   ],
 };
 const content = {
   title: "Contenu",
+  id: "content",
   items: [
-    { title: "Nature du contrat/abonnement", name: "cFile" },
-    { title: "Date de début", name: "cStartDate" },
-    { title: "Date de fin", name: "cEndDate" },
-    { title: "Numéro de contrat/abonnement", name: "cFileNumber" },
-    { title: "Raison de la décision", name: "cReason" },
+    {
+      title: "Nature du contrat/abonnement",
+      name: "cFile",
+      icon: "edit",
+      fullRow: true,
+    },
+    // {
+    //   title: "Date de début",
+    //   name: "cStartDate",
+    //   icon: "timeline-events",
+    //   fullRow: true,
+    //   type: "textArea",
+    // },
+    {
+      title: "Date de début",
+      name: "cStartDate",
+      icon: "timeline-events",
+    },
+    {
+      title: "Date de fin",
+      name: "cEndDate",
+      icon: "timeline-events",
+    },
+    {
+      title: "Numéro de contrat/abonnement",
+      name: "cFileNumber",
+      icon: "numerical",
+      fullRow: true,
+    },
+    {
+      title: "Raison de la décision",
+      name: "cReason",
+      icon: "comment",
+      fullRow: true,
+      type: "textArea",
+    },
   ],
 };
 
 export const CancelB = [sender, destination, misc, content];
+export const CancelL = {
+  sender: true,
+  destination: true,
+  misc: true,
+  content: true,
+};
 
 export const CancelV = ({
   // SENDER
-  sFirstName,
-  sLastName,
-  sAddress,
-  sPostcode,
-  sCity,
-  sPhone,
-  sEmail,
+  sFirstName = "Prénom",
+  sLastName = "Nom",
+  sAddress = "Adresse",
+  sPostcode = "Code Postal",
+  sCity = "Ville",
+  sPhone = "Numéro de téléphone",
+  sEmail = "Adresse email",
   // DESTINATION
-  dName,
-  dAddress,
-  dPostcode,
-  dCity,
+  dName = "Destinataire",
+  dAddress = "Adresse",
+  dPostcode = "Code Postal",
+  dCity = "Ville",
   // MISC
-  mCity,
-  mDate,
-  mObjet,
+  mCity = "(Ville)",
+  mDate = "(Date)",
+  mObjet = "Résiliation de contrat",
   // CONTENT
-  cFile,
-  cStartDate,
-  cEndDate,
-  cFileNumber,
-  cReason,
+  cFile = "(contrat/abonnement)",
+  cStartDate = "(date de début)",
+  cEndDate = "(date de fin)",
+  cFileNumber = "(numéro du contrat/abonnement)",
+  cReason = "(raison de la décision)",
   ref,
 }: any) => (
   <Div>
-    <div className="outer-header x" />
-    <div className="container x">
-      <div className="inner-header x" />
+    <div className="outer-header" />
+    <div className="container">
+      <div className="inner-header" />
 
-      <div className="photo x" />
-      <div className="sender-names x" ref={ref}>
-        <span className="sender-first-name x">{sFirstName}</span>
+      <div className="photo" />
+      <div className="sender-names" ref={ref}>
+        <span className="sender-first-name">{sFirstName}</span>
         <span> </span>
-        <span className="sender-last-name x">{sLastName}</span>
+        <span className="sender-last-name">{sLastName}</span>
       </div>
-      <span className="sender-address x">{sAddress}</span>
-      <span className="sender-postcode x">{sPostcode}</span>
-      <span className="sender-city x">{sCity}</span>
-      <span className="sender-phone x">{sPhone}</span>
-      <span className="sender-email x">{sEmail}</span>
+      <span className="sender-address">{sAddress}</span>
+      <span className="sender-postcode">{sPostcode}</span>
+      <span className="sender-city">{sCity}</span>
+      <span className="sender-phone">{sPhone}</span>
+      <span className="sender-email">{sEmail}</span>
 
-      <span className="destin-name x">{dName}</span>
-      <span className="destin-address x">{dAddress}</span>
-      <span className="destin-postcode x">{dPostcode}</span>
-      <span className="destin-city x">{dCity}</span>
+      <span className="destin-name">{dName}</span>
+      <span className="destin-address">{dAddress}</span>
+      <span className="destin-postcode">{dPostcode}</span>
+      <span className="destin-city">{dCity}</span>
 
-      <span className="done-at x">
+      <span className="done-at">
         Faite à {mCity}, le {mDate}
       </span>
 
-      <span className="object-info x">Object: {mObjet}</span>
+      <span className="object-info">Object: {mObjet}</span>
 
-      <div className="content x">
+      <div className="content">
         <p>Madame, Monsieur,</p>
         <p>
           Par la présente, je vous fais part de ma volonté de résilier mon{" "}
@@ -125,11 +196,11 @@ export const CancelV = ({
         </p>
       </div>
 
-      <p className="signature x">Signature</p>
+      <p className="signature">Signature</p>
 
-      <div className="inner-footing x" />
+      <div className="inner-footing" />
     </div>
-    <div className="outer-footing x" />
+    <div className="outer-footing" />
   </Div>
 );
 
--- src/models/Test.tsx
+++ src/models/Test.tsx
@@ -5,7 +5,33 @@ type TestVType = {
   test: string;
 };
 
-export const TestB = [{ title: "Test", name: "test" }];
+const test1 = {
+  title: "Test 1",
+  id: "test1",
+  items: [
+    { title: "test1", name: "test1", icon: "person" },
+    { title: "test2", name: "test2", icon: "person" },
+  ],
+};
+const test2 = {
+  title: "Test 2",
+  id: "test2",
+  items: [
+    {
+      title: "test2",
+      name: "test2",
+      icon: "map-marker",
+    },
+    {
+      title: "test2",
+      name: "test2",
+      icon: "calendar",
+    },
+    { title: "test2", name: "test2", icon: "edit", fullRow: true },
+  ],
+};
+
+export const TestB = [test1, test2];
 
 export const TestV = ({ test }: any) => <div>test</div>;
 
