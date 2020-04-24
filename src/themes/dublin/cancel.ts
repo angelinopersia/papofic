a6ceb745befb89c64b17cf9234d02fb5550c4329
Steven Persia <persia.steven8@gmail.com>
2020-04-24 17:40:02 +0200

    feat(Dublin): create Dublin theme

---
+++ src/themes/dublin/cancel.ts
@@ -0,0 +1,48 @@
+const StyledView = {
+  ul: {
+    margin: 0,
+    padding: 0,
+    listStyleType: "none",
+  },
+
+  ".container": {
+    padding: 25,
+    fontFamily: "Calibri",
+  },
+
+  ".top": {},
+  ".bottom": {
+    width: 794,
+    height: 500,
+    background: "#ade4c5",
+    position: "absolute",
+    transform: "skewY(-11deg)",
+  },
+  ".left": {},
+  ".right": {},
+
+  ".head": { display: "grid" },
+  ".sender": {},
+  ".destin": { textAlign: "right" },
+
+  ".done-at": {
+    paddingTop: 20,
+    textAlign: "right",
+  },
+
+  ".object-info": {
+    padding: "40px 0",
+    fontSize: 18,
+  },
+
+  ".content": {
+    textAlign: "justify",
+  },
+
+  ".signature": {
+    paddingTop: 100,
+    fontStyle: "italic",
+  },
+};
+
+export default StyledView;
